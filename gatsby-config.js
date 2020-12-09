26a78d190991a7bd82064bb910b21637bd1b34c6
sparkdigital <g@withspark.digital>
2020-12-09 08:29:18 -0600

    MD slugs

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SparkBlog`,
+    description: `Grow your business online.`,
+    author: `Spark Digital`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -13,6 +13,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/src/blog/`,
       },
     },
     `gatsby-transformer-sharp`,
