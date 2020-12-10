ef45479d5cb2d78259e041755130b025a4f928cd
sparkdigital <g@withspark.digital>
2020-12-09 18:30:21 -0600

    images width

--- src/template/post.js
+++ src/template/post.js
@@ -6,7 +6,7 @@ export default ({ data }) => {
     return (
     <div>
         <h1>{data.airtable.data.title}</h1>
-        <img src={data.airtable.data.image[0].url} alt={data.airtable.data.title} />
+        <img src={data.airtable.data.image[0].url} alt={data.airtable.data.title} width="100%" />
         <p>{data.airtable.data.PostMarkdown}</p>
     </div>
     )
